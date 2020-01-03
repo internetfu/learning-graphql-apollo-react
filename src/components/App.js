0c6fded75be949a7e281bbca5276ea8127a95f54
Zheng Li <zhengli@Zhengs-MacBook-Air.local>
2020-01-03 20:54:54 +0100

    add routing

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+/* import LinkList from './LinkList'; */
+import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
+import { Switch, Route } from 'react-router-dom';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="center w85">
+                <Header></Header>
+                <div className="ph3 pv1 background-gray">
+                    <Switch>
+                        <Route exact path="/" component={LinkList}></Route>
+                        <Route
+                            exact
+                            path="/create"
+                            component={CreateLink}
+                        ></Route>
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,8 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+<<<<<<< Updated upstream
 ReactDOM.render(<App />, document.getElementById('root'));
 
+=======
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+    <Router>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </Router>,
+    document.getElementById('root')
+);
+>>>>>>> Stashed changes
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
