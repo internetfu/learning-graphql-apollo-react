77aede2953411eb4475bdb62e3ab57a446ac07d8
Zheng Li <zhengli@Zhengs-MacBook-Air.local>
2020-01-04 21:06:28 +0100

    add vote mutation and update apollo cache

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,13 +1,76 @@
 import React, { Component } from 'react';
+import { AUTH_TOKEN } from '../constants';
+import { timeDifferenceForDate } from '../utils/index';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+
+const VOTE_MUTATION = gql`
+    mutation VoteMutation($linkId: ID!) {
+        vote(linkId: $linkId) {
+            id
+            link {
+                id
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+            user {
+                id
+            }
+        }
+    }
+`;
 
 export default class Link extends Component {
     render() {
+        const authToken = localStorage.getItem(AUTH_TOKEN);
+
         return (
-            <div>
-                <div>
-                    {this.props.link.description}({this.props.url})
+            <div className="flex mt2 items-start">
+                <div className="flex items-center">
+                    <span className="gray">{this.props.index + 1}</span>
+                    {authToken && (
+                        <Mutation
+                            mutation={VOTE_MUTATION}
+                            variables={{ linkId: this.props.link.id }}
+                            update={(inMemoryCache, { data: { vote } }) => {
+                                this.props.updateCacheAfterVote(
+                                    inMemoryCache,
+                                    vote,
+                                    this.props.link.id
+                                );
+                            }}
+                        >
+                            {voteMutation => (
+                                <div
+                                    className="ml1 gray f11"
+                                    onClick={voteMutation}
+                                >
+                                    ▲
+                                </div>
+                            )}
+                        </Mutation>
+                    )}
+                </div>
+
+                <div className="ml1">
+                    <div>
+                        {this.props.link.description}({this.props.url})
+                    </div>
+                    <div className="f6 lh-copy gray">
+                        {this.props.link.votes.length} votes | by{' '}
+                        {this.props.link.postedBy
+                            ? this.props.link.postedBy.name
+                            : 'Unknown'}{' '}
+                        {timeDifferenceForDate(this.props.link.createdAt)}
+                    </div>
                 </div>
             </div>
         );
     }
+
+    _voteForLink = () => {};
 }
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,21 +3,31 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Link from './Link';
 
-export default class LinkList extends Component {
-    render() {
-        const FEED_QUERY = gql`
-            {
-                feed {
-                    links {
+export const FEED_QUERY = gql`
+    {
+        feed {
+            links {
+                id
+                createdAt
+                url
+                description
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
                         id
-                        createdAt
-                        url
-                        description
                     }
                 }
             }
-        `;
+        }
+    }
+`;
 
+export default class LinkList extends Component {
+    render() {
         return (
             <Query query={FEED_QUERY}>
                 {({ loading, error, data }) => {
@@ -28,13 +38,31 @@ export default class LinkList extends Component {
 
                     return (
                         <div>
-                            {linksToRender.map(link => (
-                                <Link key={link.id} link={link} />
+                            {linksToRender.map((link, index) => (
+                                <Link
+                                    key={link.id}
+                                    link={link}
+                                    index={index}
+                                    updateCacheAfterVote={
+                                        this._updateCacheAfterVote
+                                    }
+                                />
                             ))}
                         </div>
                     );
                 }}
             </Query>
         );
+    }
+
+    _updateCacheAfterVote(inMemoryCache, createdVote, currentLinkId) {
+        const feedCache = inMemoryCache.readQuery({ query: FEED_QUERY });
+
+        const votedLink = feedCache.feed.links.find(
+            link => link.id === currentLinkId
+        );
+        votedLink.votes = createdVote.link.votes;
+
+        inMemoryCache.writeQuery({ query: FEED_QUERY, feedCache });
     }
 }
